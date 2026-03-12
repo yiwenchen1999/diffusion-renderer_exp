3b62dc7321d62dc0fba769270ce39343ff5e230a
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-03-12 10:27:26 -0400

    run with new data

--- run_priority_experiment.sh
+++ run_priority_experiment.sh
@@ -4,9 +4,9 @@
 set -e
 
 SOURCE_DATA="../LVSMExp/source_data_polyhaven"
-PRIORITY_LIST="priority_list"
-SUBSET_DATA="source_data_polyhaven_priority"
-WORKSPACE="polyhaven_workspace_priority"
+PRIORITY_LIST="relight_metadata_smallsplit"
+SUBSET_DATA="source_data_polyhaven_smallsplit"
+WORKSPACE="polyhaven_workspace_smallsplit"
 
 echo "=== Step 0: Create priority sub-dataset ==="
 python create_priority_subset.py \
